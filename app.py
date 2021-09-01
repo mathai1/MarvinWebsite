fa0aad173ef8031dc8ff66e700bf2bf512ee34b4
mathai1 <thaimarvin1@gmail.com>
2021-08-31 19:15:50 -0700

    styling in progress

--- app.py
+++ app.py
@@ -3,8 +3,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route("/projects")
+def project():
+    return render_template('project.html')
+
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
